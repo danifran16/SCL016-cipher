d7ecb74a9c6e8c4edd81f9cbe9b0d97a9de67f34
danifran16 <daniela.frangb@gmail.com>
2021-02-22 09:11:49 -0300

    avance primera pagina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+const siguiente = document.getElementById("H1").style.display= none;
+ 
+ function siguiente(){
+ 
+
+
+
+}
+
+
+ 
+ 
+ 
+    
+
